ca90b4c7636db1b981bf9c960f7f4fd143df7a7c
Admin <vadimsuman55@gmail.com>
2025-12-09 12:03:02 +0700

    Добавление стека возврата и NavDisplay

--- app/src/main/java/com/example/navigationdemo/MainActivity.kt
+++ app/src/main/java/com/example/navigationdemo/MainActivity.kt
@@ -12,6 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigationdemo.ui.theme.NavigationDemoTheme
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.example.navigationdemo.screens.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +37,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    val backStack = rememberNavBackStack(HomeScreen)
+    val onNavigation: (NavKey) -> Unit = {
+        backStack.add(it)
+    }
+    val onClearBackStack: () -> Unit = {
+        while (backStack.size > 1) {
+            backStack.removeLastOrNull()
+        }
+    }
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryProvider = entryProvider {
+            entry<HomeScreen> {
+                Home(onNavigation)
+            }
+            entry<WelcomeScreen>(
+                metadata = mapOf("extraDataKey" to "extraDataValue")
+            ) { key ->
+                Welcome(onNavigation, key.name)
+            }
+            entry<ProfileScreen> {
+                Profile(onClearBackStack)
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
