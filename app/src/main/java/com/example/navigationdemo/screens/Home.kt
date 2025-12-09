b70d5ceccd4a10d92f3c914227d373fa7ba57d5f
Admin <vadimsuman55@gmail.com>
2025-12-09 11:52:22 +0700

    Главный экран - home

---
+++ app/src/main/java/com/example/navigationdemo/screens/Home.kt
@@ -0,0 +1,68 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation3.runtime.NavKey
+
+@Composable
+fun Home(onNavigation: (NavKey) -> Unit) {
+    var userName by remember { mutableStateOf("") }
+    val onTextChange = { text : String ->
+        userName = text
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            CustomTextField(
+                title = "Enter your name",
+                textState = userName,
+                onTextChange = onTextChange
+            )
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(onClick = {
+            }) {
+                Text(text = "Register")
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { onTextChange(it) },
+        singleLine = true,
+        label = { Text(title)},
+        modifier = Modifier.padding(10.dp),
+        textStyle = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        )
+    )
+}
