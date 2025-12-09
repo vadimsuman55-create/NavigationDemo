a587886d76ff2a8d12fb1658070b34cf4da1eef6
Admin <vadimsuman55@gmail.com>
2025-12-09 11:54:25 +0700

    Добавление экрана приветствия Welcome

---
+++ app/src/main/java/com/example/navigationdemo/screens/Welcome.kt
@@ -0,0 +1,36 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation3.runtime.NavKey
+
+@Composable
+fun Welcome(onNavigation: (NavKey) -> Unit, name: String = "") {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                "Welcome $name",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(onClick = {
+            }) {
+                Text(text = "Set up your Profile")
+            }
+        }
+    }
+}
